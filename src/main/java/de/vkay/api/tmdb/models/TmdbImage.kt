cac93ef22d9cf400152ebd7a6e909d4a6201f651
vkay94 <viktor.krez94@gmail.com>
2021-04-18 14:32:06 +0200

    TmdbImage: Add all available image sizes to Quality

--- src/main/java/de/vkay/api/tmdb/models/TmdbImage.kt
+++ src/main/java/de/vkay/api/tmdb/models/TmdbImage.kt
@@ -19,17 +19,37 @@ data class TmdbImage internal constructor(
      *
      * @param quality Resolution of the image
      */
-    fun get(quality: Quality = Quality.W_500): String {
+    fun get(quality: Quality = Quality.ORIGINAL): String {
         return get(filePath, quality)
     }
 
     enum class Quality(private val size: String) {
-        W_92("w92"),
-        W_185("w185"),
-        W_300("w300"),
-        W_500("w500"),
-        W_780("w780"),
-        W_1280("w1280"),
+        BACKDROP_W_300("w300"),
+        BACKDROP_W_780("w780"),
+        BACKDROP_W_1280("w1280"),
+
+        LOGO_W_45("w45"),
+        LOGO_W_92("w92"),
+        LOGO_W_154("w154"),
+        LOGO_W_185("w185"),
+        LOGO_W_300("w300"),
+        LOGO_W_500("w500"),
+
+        POSTER_W_92("w92"),
+        POSTER_W_154("w154"),
+        POSTER_W_185("w185"),
+        POSTER_W_342("w342"),
+        POSTER_W_500("w500"),
+        POSTER_W_780("w780"),
+
+        PROFILE_W_45("w45"),
+        PROFILE_W_154("w154"),
+        PROFILE_H_632("h632"),
+
+        STILL_W_92("w92"),
+        STILL_W_185("w185"),
+        STILL_W_300("w300"),
+
         ORIGINAL("original");
 
         override fun toString(): String = size
--- src/test/java/ImageTest.kt
+++ src/test/java/ImageTest.kt
@@ -19,30 +19,23 @@ class ImageTest {
 
     @Test
     fun `Test TMDbImage`() {
-        val imageFull = TmdbImage("/path/to/image", 100, 200, "en")
-        val imageSecondConstructor = TmdbImage("/path/to/image")
+        val image = TmdbImage("/path/to/image")
 
-        assertEquals("/path/to/image", imageFull.filePath)
-        assertEquals(100, imageFull.height)
-        assertEquals(200, imageFull.width)
-        assertEquals("en", imageFull.languageCode)
-
-        assertEquals("/path/to/image", imageSecondConstructor.filePath)
-        assertEquals(0, imageSecondConstructor.height)
-        assertEquals(0, imageSecondConstructor.width)
-        assertEquals(null, imageSecondConstructor.languageCode)
+        assertEquals("/path/to/image", image.filePath)
+        assertEquals(0, image.height)
+        assertEquals(0, image.width)
+        assertEquals(null, image.languageCode)
     }
 
     @Test
     fun `Test TMDbImage get method and Quality`() {
         val image = TmdbImage("/path/to/image")
-        assertEquals("https://image.tmdb.org/t/p/w500/path/to/image", image.get())
-        assertEquals("https://image.tmdb.org/t/p/w92/path/to/image", image.get(TmdbImage.Quality.W_92))
-        assertEquals("https://image.tmdb.org/t/p/w185/path/to/image", image.get(TmdbImage.Quality.W_185))
-        assertEquals("https://image.tmdb.org/t/p/w300/path/to/image", image.get(TmdbImage.Quality.W_300))
-        assertEquals("https://image.tmdb.org/t/p/w500/path/to/image", image.get(TmdbImage.Quality.W_500))
-        assertEquals("https://image.tmdb.org/t/p/w780/path/to/image", image.get(TmdbImage.Quality.W_780))
-        assertEquals("https://image.tmdb.org/t/p/w1280/path/to/image", image.get(TmdbImage.Quality.W_1280))
-        assertEquals("https://image.tmdb.org/t/p/original/path/to/image", image.get(TmdbImage.Quality.ORIGINAL))
+        assertEquals("https://image.tmdb.org/t/p/original/path/to/image", image.get())
+        assertEquals("https://image.tmdb.org/t/p/w92/path/to/image", image.get(TmdbImage.Quality.POSTER_W_92))
+        assertEquals("https://image.tmdb.org/t/p/w185/path/to/image", image.get(TmdbImage.Quality.POSTER_W_185))
+        assertEquals("https://image.tmdb.org/t/p/w300/path/to/image", image.get(TmdbImage.Quality.STILL_W_300))
+        assertEquals("https://image.tmdb.org/t/p/w500/path/to/image", image.get(TmdbImage.Quality.POSTER_W_500))
+        assertEquals("https://image.tmdb.org/t/p/w780/path/to/image", image.get(TmdbImage.Quality.BACKDROP_W_780))
+        assertEquals("https://image.tmdb.org/t/p/w1280/path/to/image", image.get(TmdbImage.Quality.BACKDROP_W_1280))
     }
 }
