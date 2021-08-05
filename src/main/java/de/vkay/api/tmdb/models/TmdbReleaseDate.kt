5feaa37702d65a84ac9d58e88187e49b88a6e353
vkay94 <viktor.krez94@gmail.com>
2021-08-05 10:48:31 +0200

    TmdbReleaseDate: Change note field to nullable (optional)

--- src/main/java/de/vkay/api/tmdb/models/TmdbReleaseDate.kt
+++ src/main/java/de/vkay/api/tmdb/models/TmdbReleaseDate.kt
@@ -8,7 +8,7 @@ import de.vkay.api.tmdb.enumerations.ReleaseType
 data class TmdbReleaseDate internal constructor(
     @Json(name = "iso_639_1")
     val languageCode: String?,
-    val note: String,
+    val note: String?,
     @Json(name = "release_date")
     val releaseDate: TmdbDate,
     val certification: String,
